1c511d079db2218281fbd1e24a47177473658e1a
Leonardo <lfaoro@gmail.com>
2019-05-15 17:51:03 +0200

    misc: add copyright statement to go files

--- cmd/server/fn.go
+++ cmd/server/fn.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2019 Leonardo Faoro. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 package server
 
 //go:generate gcloud config set project ncrypt
